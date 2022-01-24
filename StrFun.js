d94d5ca1c6b73edaa2049489779dd642aa5a78e2
bsvijayan <bsvijayan@gmail.com>
2022-01-24 22:17:30 +0530

    Create StrFun.js
    String utilities

---
+++ StrFun.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!");
+var str="THis is new String"
+console.log(str)
+
+var str="This is test string".indexOf('tri');
+console.log(str)
+
+var str="This is sample String".length;
+console.log(str);
+
+var str="This is test string".substr(0,10);
+console.log(str)
+
+var str="This is sample String".split(10,15);
+console.log(str);
+
+var str="This \n is \n test string".indexOf('tri');
+console.log(str)
+
+var str="This is sample String".split(' ');
+console.log(str);
+
+var str="This is sample String".length;
+console.log(str);
